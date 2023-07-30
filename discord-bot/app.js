9e61f0f93b52ebc0273c49f5e38692b6f39303e2
Braden <bradenschulz@gmail.com>
2023-07-29 20:24:58 -0400

    Fill in automatic search skeleton, create scraping skeletons
    There's some refactoring in here too, as I clearly can't help myself.
    I'm pretty happy with how the rich embeds look, so this will probably be
    the final message design?

--- discord-bot/app.js
+++ discord-bot/app.js
@@ -3,18 +3,27 @@ import {
 	Client,
 	Events,
 	GatewayIntentBits,
+	EmbedBuilder,
 } from 'discord.js';
+import { getSite1Listings } from './scraper/site1/site1-scraper.js';
+import { getSite2Listings } from './scraper/site2/site2-scraper.js';
 
 const SEARCHWAIT_MIN = 5;						// Minimum time to wait between searches in minutes
 const SEARCHWAIT = SEARCHWAIT_MIN*60*1000;		// SEARCHWAIT_MIN in milliseconds
+const LISTINGS_PER_SEARCH = 5;
 
 let statusChannel;
 let searchChannel;
 
+// Default options block
+let searchSite1 = true;
+let searchSite2 = true;
+
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.login(process.env.DISCORD_TOKEN);
 
-// Note in console and status channel and begin searching once ready
+// Startup routine: Notify in appropriate channels and begin searching
 client.once(Events.ClientReady, c => {
 	console.log(`Successfully started logged in as ${c.user.tag}`);
 
@@ -26,11 +35,61 @@ client.once(Events.ClientReady, c => {
 	statusChannel.send('Started!');
 	sendListings();
 	// Wait SEARCHWAIT plus random timelength between [1, SEARCHWAIT] between each search (to seem human)
-	setInterval(sendListings, SEARCHWAIT + Math.floor(Math.random() * SEARCHWAIT) + 1);
+	//setInterval(sendListings, SEARCHWAIT + Math.floor(Math.random() * SEARCHWAIT) + 1);
 });
 
-// sendListings(): Scrape for new listings and send any that are found to the search channel
+
+/********************************************************************************************************
+ * @brief Scrapes for new listings and sends any that are found to the search channel.
+ ********************************************************************************************************/
 function sendListings() {
 	console.log('Searching for listings...');
 	statusChannel.send('Searching...');
+
+	if (searchSite1) {
+		let site1Listings = getSite1Listings();
+		let site1Embeds = [];
+		
+		// Propagate embed array with scraped listing data
+		for (let i = 0; i < LISTINGS_PER_SEARCH; ++i) {
+			if (site1Listings[i] === undefined) 
+				continue;
+
+			const currentEmbed = new EmbedBuilder()
+				.setColor(0xc4c4c4)
+				.setTitle(site1Listings[i].title)
+				.addFields({ name: site1Listings[i].subheading, value: site1Listings[i].description})
+				.setThumbnail(site1Listings[i].image)
+				.setURL(site1Listings[i].url)
+				.setFooter({ text: site1Listings[i].postDate})
+				.setTimestamp()
+			site1Embeds.push(currentEmbed);
+		}
+
+		if (site1Embeds.length > 0)
+			searchChannel.send({ content: '# Site 1', embeds: site1Embeds });
+	}
+	
+	if (searchSite2) {
+		let site2Listings = getSite2Listings();
+		let site2Embeds = [];
+		
+		for (let i = 0; i < LISTINGS_PER_SEARCH; ++i) {
+			if (site2Listings[i] === undefined) 
+				continue;
+
+			const currentEmbed = new EmbedBuilder()
+				.setColor(0xc4c4c4)
+				.setTitle(site2Listings[i].title)
+				.addFields({ name: site2Listings[i].subheading, value: site2Listings[i].description})
+				.setThumbnail(site2Listings[i].image)
+				.setURL(site2Listings[i].url)
+				.setFooter({ text: site2Listings[i].postDate})
+				.setTimestamp()
+			site2Embeds.push(currentEmbed);
+		}
+
+		if (site2Embeds.length > 0)
+			searchChannel.send({ content: '# Site 2', embeds: site2Embeds });
+	}
 }
---
+++ discord-bot/scraper/site2/site2-scraper.js
@@ -0,0 +1,30 @@
+/********************************************************************************************************
+ * @brief Scrapes and returns an array of some website's listings with a maximum length of 
+ * 		  LISTINGS_PER_SEARCH. Most recently posted listings are returned first, with any marked as seen
+ * 		  being discarded. In debug mode, no listings are discarded regardless of their status or content.
+ ********************************************************************************************************/
+export function getSite2Listings() {
+	let listings = [];
+	
+	let newListing = {
+		title: 'First Site2 listing',
+		// url: 'link-to-listing',
+		image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png',
+		subheading: 'Listing subheader',
+		description: 'Some sort of description...',
+		postDate: 'Posted at some point' 
+	}
+	listings.push(newListing);
+
+	newListing = {
+		title: 'Second Site2 listing',
+		// url: 'link-to-listing'
+		image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png',
+		subheading: 'Listing subheader',
+		description: 'Some sort of description...',
+		postDate: 'Posted at some point'
+	}
+	listings.push(newListing);
+
+	return listings;
+}
